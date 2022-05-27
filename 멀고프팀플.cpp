e09e9c10d507845b8d6fc24ec105cad82bd62b4f
dongmin0204 <84498742+dongmin0204@users.noreply.github.com>
2022-05-27 22:15:32 +0900

    Update 멀고프팀플.cpp

--- 멀고프팀플.cpp
+++ 멀고프팀플.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
+#include <fstream>
 #include <string>
+#include <vector>
+#include <sstream>
 
 using namespace std;
 
-class Subject{
+class Subject {
 public:
     string type; //전공,교양 구분
     int score; //원점수
@@ -12,26 +15,26 @@ public:
     void Grading();
 };
 
-void Subject::Grading(){
-    if(score>=90)
-        grade='A';
-    else if(score<90&&score>=80)
-        grade='B';
-    else if(score<80&&score>=70)
-        grade='C';
-    else if(score<70&&score>=60)
-        grade='D';
+void Subject::Grading() {
+    if (score >= 90)
+        grade = 'A';
+    else if (score < 90 && score >= 80)
+        grade = 'B';
+    else if (score < 80 && score >= 70)
+        grade = 'C';
+    else if (score < 70 && score >= 60)
+        grade = 'D';
     else
-        grade='F';
+        grade = 'F';
 }
 
-class Student{
+class Student {
 public:
     string name;
-    Subject* sbj=new Subject;
+    Subject* sbj = new Subject;
 };
 
-void printMenu(void){
+void printMenu(void) {
     cout << "------------------------------" << endl;
     cout << "1. 성적 입력" << endl;
     cout << "2. 개인 성적 출력" << endl;
@@ -42,46 +45,81 @@ void printMenu(void){
     cout << "------------------------------" << endl;
 }
 
-void addStudent(Student student[15]){
-    for(int i=0; i<15; i++){
+void addStudent(Student student[15]) {
+    for (int i = 0; i < 15; i++) {
         string name;
         cin >> name;
-        student[i].name=name;
+        student[i].name = name;
     }
 }
 
-int main(){
+void addfstudent(Student student[15]) {
+
+    vector<vector<string>> content;
+    vector<string> row;
+    string line, word;
+
+    fstream file("test.csv", ios::in);
+    if (file.is_open())
+    {
+        while (getline(file, line))
+        {
+            row.clear();
+
+            stringstream str(line);
+
+            while (getline(str, word, ','))
+                row.push_back(word);
+            content.push_back(row);
+        }
+    }
+    else
+        cout << "Could not open the file\n";
+
+    for (int i = 0; i < content.size(); i++)
+    {
+        student[i].name = content[i][1];
+    }
+}
+
+int main() {
     Student student[15];
-    int cmd=0;
-    do{
+    int cmd = 0;
+
+    
+
+    do {
         printMenu();
         cout << "메뉴 선택 >> ";
         cin >> cmd;
         switch (cmd) {
-            case 1:
-                addStudent(student);
-                cout << "1. 성적 입력" << endl;
-                break;
-            case 2:
-                cout << "2. 개인 성적 출력" << endl;
-                break;
-            case 3:
-                cout << "3. 전체 성적 출력" << endl;
-                break;
-            case 4:
-                cout << "4. 학기 별 등수 출력" << endl;
-                break;
-            case 5:
-                cout << "5. 과목별 상위 3명 출력" << endl;
-                break;
-            case 6:
-                break;
-            default:
-                cout << "입력 오류 ! 다시 입력해주세요..." << endl;
-                break;
+        case 1:
+            cout << "1. 성적 입력" << endl;
+            cout << "test.csv 파일을 읽어 오겠습니다... " << endl;
+            addfstudent(student);
+            for (int i = 0; i < 15; i++) { cout << student[i].name << " "; }
+            cout << endl;
+            break;
+        case 2:
+            cout << "2. 개인 성적 출력" << endl;
+            break;
+        case 3:
+            cout << "3. 전체 성적 출력" << endl;
+            break;
+        case 4:
+            cout << "4. 학기 별 등수 출력" << endl;
+            break;
+        case 5:
+            cout << "5. 과목별 상위 3명 출력" << endl;
+            break;
+        case 6:
+            break;
+        default:
+            cout << "입력 오류 ! 다시 입력해주세요..." << endl;
+            break;
         }
-    }while(cmd!=6);
+    } while (cmd != 6);
     cout << "프로그램을 종료합니다." << endl;
-    
+
     return 0;
 }
